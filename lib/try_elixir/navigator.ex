3a6d675cbd022513e6bf4fe8dae9b703d936e201
Wende Lu <wendelu1991@gmail.com>
2019-09-21 15:14:09 +0800

    fix navigator directory type

--- lib/try_elixir/navigator.ex
+++ lib/try_elixir/navigator.ex
@@ -12,21 +12,27 @@ defmodule Navigator do
   def go_through([], _pre_path, _level), do: nil
 
   def go_through([head | tail], pre_path, level) do
-    print(head, level)
-
     combined_relative_path = "#{pre_path}/#{head}"
     expanded_path = Path.expand(combined_relative_path)
+    is_directory = dir?(expanded_path)
 
-    if File.dir?(expanded_path) do
+    print(head, level, is_directory)
+
+    if is_directory do
       navigate(combined_relative_path, level + 1)
     end
 
     go_through(tail, pre_path, level)
   end
 
-  def print(target, level) do
+  def print(target, level, is_directory) do
     target_space = List.duplicate(" ", level) |> Enum.join()
-    target_type = if(File.dir?(target), do: "+", else: "-")
+    target_type = if(is_directory, do: "+", else: "-")
     IO.puts("#{target_space}#{target_type} #{target}")
+  end
+
+  def dir?(dir) do
+    {:ok, %{type: type}} = File.lstat(dir)
+    type == :directory
   end
 end

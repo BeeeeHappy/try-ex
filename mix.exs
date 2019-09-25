2720a5cbf8e603eb90212956b1656a59297831fe
Wende Lu <wendelu1991@gmail.com>
2019-09-26 06:54:11 +0800

    add dialyzer

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule TryElixir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
     ]
   end
 end

61f765c4870d9f335141307fa58a03e5c337d747
Wende Lu <wendelu1991@gmail.com>
2019-09-30 11:23:28 +0800

    format

--- lib/otp/worker_collector.ex
+++ lib/otp/worker_collector.ex
@@ -1,5 +1,6 @@
 defmodule WorkerCollector do
   def call([], _f), do: []
+
   def call(list, f) do
     collector = spawn()
     map(list, f, collector)
@@ -7,42 +8,45 @@ defmodule WorkerCollector do
   end
 
   defp map([], _f, _collector), do: []
+
   defp map([head | tail], f, collector) do
     worker = spawn
-    send worker, {head, f}
+    send(worker, {head, f})
     map(tail, f, collector)
   end
 
   defp receive_r do
-    IO.puts "receiving result..."
+    IO.puts("receiving result...")
+
     receive do
       {:ok, r} -> r
       :error -> :error
     end
     |> inspect
-    |> IO.puts
+    |> IO.puts()
   end
 end
 
 defmodule WorkerCollector.Worker do
   def start(collector, e, f) do
     v = f.(e)
-    send collector, {:ok, v}
+    send(collector, {:ok, v})
   end
 end
 
 defmodule WorkerCollector.Collector do
   def loop(pid, count, r \\ []) do
-    new_r = 
+    new_r =
       receive do
         {:ok, v} -> [v | r]
         :error -> r
       end
 
     new_count = count - 1
+
     if new_count == 0 do
-      sorted_r = new_r |> Enum.sort
-      send pid, {:ok, sorted_r}
+      sorted_r = new_r |> Enum.sort()
+      send(pid, {:ok, sorted_r})
     else
       loop(pid, new_count, new_r)
     end

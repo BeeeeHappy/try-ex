b823891b55c4c579ec203e2648dfdd56b23ffab1
Wende Lu <wendelu1991@gmail.com>
2019-10-04 08:48:05 +0800

    g

---
+++ lib/otp/g.ex
@@ -0,0 +1,35 @@
+defmodule Otp.G do
+  use GenServer
+
+  def start_link() do
+    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def stack do
+    GenServer.call(__MODULE__, :stack)
+  end
+
+  def pop do
+    GenServer.call(__MODULE__, :pop)
+  end
+
+  def push(item) do
+    GenServer.call(__MODULE__, {:push, item})
+  end
+
+  def init(stack) when is_list(stack) do
+    {:ok, stack}
+  end
+
+  def handle_call(:stack, from, stack) do
+    {:reply, stack, stack}
+  end
+
+  def handle_call(:pop, _from, [head | tail]) do
+    {:reply, head, tail}
+  end
+
+  def handle_call({:push, item}, _from, stack) do
+    {:reply, :ok, [item | stack]}
+  end
+end
